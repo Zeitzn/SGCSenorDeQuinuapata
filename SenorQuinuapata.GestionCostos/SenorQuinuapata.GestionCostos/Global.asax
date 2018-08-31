7781d8e7261a4895734027fc0a8b5a355a08e653
Isac <smevy4lc3@gmail.com>
2018-08-31 18:28:08 -0500

    creacion del proyecto

---
+++ SenorQuinuapata.GestionCostos/SenorQuinuapata.GestionCostos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SenorQuinuapata.GestionCostos.MvcApplication" Language="C#" %>
---
+++ SenorQuinuapata.GestionCostos/SenorQuinuapata.GestionCostos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SenorQuinuapata.GestionCostos.Startup))]
+namespace SenorQuinuapata.GestionCostos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
