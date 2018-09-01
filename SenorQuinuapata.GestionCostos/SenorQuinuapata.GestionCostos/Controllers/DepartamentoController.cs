f2c7dc2334c5f583fbcc82d788cfcd1f54061c73
Isac <smevy4lc3@gmail.com>
2018-09-01 01:30:12 -0500

    form ingresos

---
+++ SenorQuinuapata.GestionCostos/SenorQuinuapata.GestionCostos/Controllers/DepartamentoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SenorQuinuapata.GestionCostos.Controllers
+{
+    public class DepartamentoController : Controller
+    {
+        // GET: Registro
+        public ActionResult Ingreso()
+        {
+            return View();
+        }
+    }
+}
